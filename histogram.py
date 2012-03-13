3fa782e80ab4cd89150fb2ed744202612da0ea66
James Guillochon <jfg@ucolick.org>
2012-03-12 17:36:44 -0700

    First commit of a few helpful YT scripts.

---
+++ histogram.py
@@ -0,0 +1,230 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='Generate a histogram plot of FLASH data.')
+parser.add_argument('filename',                                 help='filename', metavar='filename', nargs='+')
+parser.add_argument('--parallel',           dest='parallel',    help='run in parallel', default=False, action='store_true')
+parser.add_argument('--var', '-v',          dest='var',         help='variable', default='Density', type=str)
+parser.add_argument('--excludevars', '-ev', dest='excludevars', help='exclude where these variables is greater than some threshold', default=[], type=str, nargs='+')
+parser.add_argument('--excludethr', '-et',  dest='excludethr',  help='Threshold values for exclusions', default=[0.], type=float, nargs='+')
+parser.add_argument('--excludetype', '-ey', dest='excludetype', help='Threshold type for exclusions', default=['max'], type=str, nargs='+')
+parser.add_argument('--log', '-l',          dest='log',         help='take the log of the variable', action='store_true', default=False)
+parser.add_argument('--rotstep', '-rs',     dest='rotstep',     help='angle in degrees between rotations', default=0., type=float)
+parser.add_argument('--rotdir', '-rd',      dest='rotdir',      help='normal vector of axis to rotate about', nargs='+', default=[1.,0.,0.], type=float)
+parser.add_argument('--res', '-r',          dest='res',         help='image resolution', default=512, type=int)
+parser.add_argument('--min',                dest='min',         help='minimum value to plot', type=float)
+parser.add_argument('--max',                dest='max',         help='maximum value to plot', type=float)
+parser.add_argument('--nbins', '-n',        dest='nbins',       help='number of bins to include', type=int, default=100)
+parser.add_argument('--negative',           dest='negative',    help='plots -1.0*value', action='store_true', default=False)
+parser.add_argument('--silent',             dest='silent',      help='no plot windows generated', action='store_true', default=False)
+parser.add_argument('--fileprefix', '-fp',  dest='fileprefix',  help='prefix for output filename', type=str, default='')
+parser.add_argument('--outputfile', '-o',   dest='outputfile',  help='explicitly set output filename', type=str, default='')
+parser.add_argument('--minradius',  '-mr',  dest='minradius',   help='minimum distance from point mass to consider, scaled to the pericenter distance', type=float, default=0.)
+parser.add_argument('--undersample', '-u',  dest='undersample', help='do not include blocks this many level away from maximum or less', type=int, default=0)
+parser.add_argument('--oversample', '-ov',  dest='oversample',  help='interpolate maximally refined blocks by this many levels', type=int, default=0)
+parser.add_argument('--subsample', '-s',    dest='subsample',   help='sample lower res blocks within this number of levels of maximum refinement', type=int, default=0)
+args = parser.parse_args()
+
+from yt.config import ytcfg; ytcfg["yt","serialize"] = "False"
+from yt.mods import *
+import pylab as pl
+import string
+from jYT import congrid
+from progressbar import Bar, ETA, Percentage, ProgressBar
+
+if (args.negative) :
+	sign = -1.0
+else :
+	sign = 1.0
+
+nghost = 1
+
+if (len(args.excludevars) > 0):
+	if len(args.excludevars) != len(args.excludethr) or len(args.excludevars) != len(args.excludetype):
+		print 'ERROR: Exclude variable names, threshold values, and types must all be the same length.'
+		sys.exit()
+
+if (args.var == 'kine') :
+	def _var(field, data):
+		return sign*0.5*data['dens']*(data['velx']**2 + data['vely']**2 + data['velz']**2)
+elif (args.var == 'vel2') :
+	def _var(field, data):
+		return sign*(data['velx']**2 + data['vely']**2 + data['velz']**2)
+elif (args.var == 'bhbound') :
+	g = 6.67428e-8
+	msun = 1.9889225e33
+
+	odata = na.loadtxt('pruned_orbit.dat', dtype='float64')
+	ptvec = odata[:,1:7]
+	obvec = odata[:,7:13]
+	totvec = odata[:,19:25]
+	peakvec = odata[:,25:31]
+	time = odata[:,0]
+
+	ptvec += totvec - obvec
+
+	edata = na.loadtxt('extras.dat', dtype='float64')
+	gmpt = g*edata[6]
+	peridist = edata[3]*na.power(edata[6]/edata[4]/msun, 1./3.)
+
+	def _var(field, data):
+		pos = na.zeros(data['x'].shape, dtype='float64')
+		vel2 = pos.copy()
+		for i, ax in enumerate(['x','y','z']) :
+			pos += (data[ax] - ptvec[tindex,i])**2.
+		for i, ax in enumerate(['velx', 'vely', 'velz']) :
+			vel2 += (data[ax] - ptvec[tindex,i+3])**2.
+		na.sqrt(pos, pos)	
+		#arr = sign*data['dens']*(gmpt/pos - 0.5*vel2)
+		arr = sign*(gmpt/pos - 0.5*vel2)
+		#arr[pos < args.minradius*peridist] = float("nan")
+		return arr
+else :
+	def _var(field, data):
+		return sign*data[args.var]
+
+
+for e, ev in enumerate(args.excludevars):
+	if (ev == 'selfbound'):
+		def _excludevar(field, data):
+			vel2 = na.zeros(data['x'].shape, dtype='float64')
+			for i, ax in enumerate(['velx', 'vely', 'velz']) :
+				vel2 += (data[ax] - peakvec[tindex,i+3])**2.
+			arr = -data['gpot'] - 0.5*vel2
+			#arr[pos < args.minradius*peridist] = float("nan")
+			return arr
+	else:
+		def _excludevar(field, data):
+			return data[ev]
+	add_field("excludevar"+str(e), function=_excludevar, take_log=args.log)
+
+add_field("var", function=_var, take_log=args.log)
+
+#my_storage = {}
+for f in args.filename:
+#for sto, f in parallel_objects(args.filename, 8, storage = my_storage):
+	pf = load(f)
+
+	tindex = abs(time - pf.current_time).argmin()
+
+	if args.subsample >= 0 and pf.h.max_level - args.undersample < args.subsample:
+		print 'ERROR: Subsample must be less than max refine level - undersample.'
+		sys.exit()
+
+	maxval = -float("inf")
+	minval = float("inf")
+	vals = list()
+	pbar = ProgressBar(widgets=['Determining histogram bounds and initial pass of data: ', Percentage(), Bar(), ' ', ETA()], maxval=len(pf.h.grids)).start()
+	for cnt, g in enumerate(pf.h.grids):
+		if g.Level > pf.h.max_level - args.undersample: continue
+		if len(g.Children) != 0 and g.Level != pf.h.max_level - args.undersample: continue
+
+		evals = list()
+		vvals = g.get_data("var").ravel()
+		dvals = g.get_data("dens").ravel()
+		for e in range(len(args.excludevars)):
+			evals.append(g.get_data("excludevar"+str(e)).ravel())
+		for e in range(len(evals)):
+			if len(evals[e]) == 0: continue
+			if args.excludetype[e] == 'min':
+				dvals = dvals[evals[e] > args.excludethr[e]]
+				vvals = vvals[evals[e] > args.excludethr[e]]
+				for e2, eval2 in enumerate(evals[e+1:]):
+					evals[e+e2+1] = eval2[evals[e] > args.excludethr[e]]
+			else:
+				dvals = dvals[evals[e] < args.excludethr[e]]
+				vvals = vvals[evals[e] < args.excludethr[e]]
+				for e2, eval2 in enumerate(evals[e+1:]):
+					evals[e+e2+1] = eval2[evals[e] < args.excludethr[e]]
+		if len(vvals) > 0:
+			if args.subsample < 0:
+				ss = max(pf.h.max_level + args.subsample - g.Level, 0)
+			else:
+				ss = min(pf.h.max_level - g.Level, args.subsample) + args.oversample
+
+			lmax = na.nanmax(vvals)
+			lmin = na.nanmin(vvals)
+			if lmax > maxval: maxval = lmax
+			if lmin < minval: minval = lmin
+			if ss == 0:
+				vals.append([vvals,dvals,na.product(g.dds)])
+		pbar.update(cnt+1)
+	pbar.finish()
+
+	histbins = na.arange(minval, maxval, (maxval-minval)/(args.nbins + 1))
+
+	hist = na.zeros(len(histbins) - 1)
+	if len(vals) > 0:
+		pbar = ProgressBar(widgets=['Binning raw data: ', Percentage(), Bar(), ' ', ETA()], maxval=len(vals)).start()
+		for cnt, val in enumerate(vals):
+			hist += val[2]*na.histogram(val[0], bins=histbins, weights=val[1])[0]
+			pbar.update(cnt+1)
+		pbar.finish()
+	else:
+		print 'No cells satisfy cuts in initial pass!'
+
+	pbar = ProgressBar(widgets=['Secondary pass of data with sub-sampling: ', Percentage(), Bar(), ' ', ETA()], maxval=len(pf.h.grids)).start()
+	for cnt, g in enumerate(pf.h.grids):
+		if g.Level > pf.h.max_level - args.undersample: continue
+		if len(g.Children) != 0 and g.Level != pf.h.max_level - args.undersample: continue
+
+		if args.subsample < 0:
+			ss = max(pf.h.max_level + args.subsample - g.Level, 0)
+		else:
+			ss = min(pf.h.max_level - g.Level, args.subsample) + args.oversample
+
+		evals = list()
+		if (ss != 0):
+			excludelist = list()
+			for e in range(len(args.excludevars)):
+				excludelist.append("excludevar"+str(e))
+			varstrings = ["var", "dens"] + excludelist
+			gz = g.retrieve_ghost_zones(nghost, varstrings, smoothed=False)
+			ssg = 2**(ss - 1)*nghost
+			cgshape = 2**ss*(na.array(na.shape(gz["var"])) - 1) + 1
+			ofs = 2.**(-ss-1)
+			vvals = congrid(gz["var"], cgshape, minusone=True, offset=ofs, buffer=ssg).ravel()
+			dvals = congrid(gz["dens"], cgshape, minusone=True, offset=ofs, buffer=ssg).ravel()
+			for e in range(len(args.excludevars)):
+				evals.append(congrid(gz["excludevar"+str(e)], cgshape, minusone=True, offset=ofs, buffer=ssg).ravel())
+		for e in range(len(evals)):
+			if len(evals[e]) == 0: continue
+			if args.excludetype[e] == 'min':
+				dvals = dvals[evals[e] > args.excludethr[e]]
+				vvals = vvals[evals[e] > args.excludethr[e]]
+				for e2, eval2 in enumerate(evals[e+1:]):
+					evals[e+e2+1] = eval2[evals[e] > args.excludethr[e]]
+			else:
+				dvals = dvals[evals[e] < args.excludethr[e]]
+				vvals = vvals[evals[e] < args.excludethr[e]]
+				for e2, eval2 in enumerate(evals[e+1:]):
+					evals[e+e2+1] = eval2[evals[e] < args.excludethr[e]]
+		if len(vvals) > 0:
+			vol = na.product(g.dds)/2**(3*ss)
+			hist += vol*na.histogram(vvals, bins=histbins, weights=dvals)[0]
+		pbar.update(cnt+1)
+	pbar.finish()
+
+	histbins = histbins[0:-1] + (histbins[1:] - histbins[0:-1])/2.
+
+	if args.outputfile != '':
+		fn = args.outputfile
+	else:
+		fn = args.var+'_histogram_'+f+'.dat'
+		if args.fileprefix != '':
+			fn = args.fileprefix+'_'+fn
+
+	#output histogram here
+	f = open(fn, 'w')
+	f.write(str(args.nbins)+'\n')
+	f.write(str(na.sum(hist))+'\n')	
+	f.write(str(time[tindex])+'\n')
+	f.write(string.join(["% 15.10E" % x for x in -histbins])+'\n')
+	f.write(string.join(["% 15.10E" % x for x in hist])+'\n')
+	f.close()
+
+	if not args.silent:
+		lhist = na.log10(hist)
+		pl.plot(histbins, lhist)
+		pl.draw()
+		pl.show()
+
---
+++ jYT.py
@@ -0,0 +1,107 @@
+import numpy as na
+import scipy.interpolate
+import scipy.ndimage
+
+def congrid(a, newdims, method='linear', centre=False, minusone=False, offset=0., buffer=0):
+	'''Arbitrary resampling of source array to new dimension sizes.
+	Currently only supports maintaining the same number of dimensions.
+	To use 1-D arrays, first promote them to shape (x,1).
+	
+	Uses the same parameters and creates the same co-ordinate lookup points
+	as IDL''s congrid routine, which apparently originally came from a VAX/VMS
+	routine of the same name.
+
+	method:
+	neighbour - closest value from original data
+	nearest and linear - uses n x 1-D interpolations using
+						 scipy.interpolate.interp1d
+	(see Numerical Recipes for validity of use of n 1-D interpolations)
+	spline - uses ndimage.map_coordinates
+
+	centre:
+	True - interpolation points are at the centres of the bins
+	False - points are at the front edge of the bin
+
+	minusone:
+	For example- inarray.shape = (i,j) & new dimensions = (x,y)
+	False - inarray is resampled by factors of (i/x) * (j/y)
+	True - inarray is resampled by(i-1)/(x-1) * (j-1)/(y-1)
+	This prevents extrapolation one element beyond bounds of input array.
+	'''
+	if not a.dtype in [na.float64, na.float32]:
+		a = na.cast[float](a)
+
+	m1 = na.cast[int](minusone)
+	ofs = na.cast[int](centre) * 0.5
+	old = na.array( a.shape )
+	ndims = len( a.shape )
+	if len( newdims ) != ndims:
+		print "[congrid] dimensions error. " \
+			  "This routine currently only support " \
+			  "rebinning to the same number of dimensions."
+		return None
+	newdims = na.asarray( newdims, dtype=float )
+	dimlist = []
+
+	if method == 'neighbour':
+		for i in range( ndims ):
+			base = na.indices(newdims)[i]
+			dimlist.append( (old[i] - m1) / (newdims[i] - m1) \
+							* (base + ofs) - ofs - offset)
+		cd = na.array( dimlist ).round().astype(int)
+		newa = a[list( cd )]
+		return newa
+
+	elif method in ['nearest','linear']:
+		# specify old dims
+		olddims = [na.arange(i, dtype = na.float) for i in list( a.shape )]
+		# calculate new dims
+		for i in range( ndims ):
+			base = na.arange( buffer+1, newdims[i]-buffer )
+			newval = (old[i] - m1) / (newdims[i] - m1) \
+							* (base + ofs) - ofs - offset
+			newval = [t for t in newval if t >= olddims[-1][0] and t <= olddims[-1][-1]]
+			dimlist.append( newval ) 
+
+		# first interpolation - for ndims = any
+		mint = scipy.interpolate.interp1d( olddims[-1], a, kind=method )
+		newa = mint( dimlist[-1] )
+
+		trorder = [ndims - 1] + range( ndims - 1 )
+		for i in range( ndims - 2, -1, -1 ):
+			newa = newa.transpose( trorder )
+
+			mint = scipy.interpolate.interp1d( olddims[i], newa, kind=method )
+			newa = mint( dimlist[i] )
+
+		if ndims > 1:
+			# need one more transpose to return to original dimensions
+			newa = newa.transpose( trorder )
+
+		return newa
+	elif method in ['spline']:
+		oslices = [ slice(0,j) for j in old ]
+		oldcoords = na.ogrid[oslices]
+		nslices = [ slice(0,j) for j in list(newdims) ]
+		newcoords = na.mgrid[nslices]
+
+		newcoords_dims = range(na.rank(newcoords))
+		#make first index last
+		newcoords_dims.append(newcoords_dims.pop(0))
+		newcoords_tr = newcoords.transpose(newcoords_dims)
+		# makes a view that affects newcoords
+
+		newcoords_tr += ofs
+
+		deltas = (na.asarray(old) - m1) / (newdims - m1)
+		newcoords_tr *= deltas
+
+		newcoords_tr -= ofs
+
+		newa = scipy.ndimage.map_coordinates(a, newcoords)
+		return newa
+	else:
+		print "Congrid error: Unrecognized interpolation type.\n", \
+			  "Currently only \'neighbour\', \'nearest\',\'linear\',", \
+			  "and \'spline\' are supported."
+		return None
