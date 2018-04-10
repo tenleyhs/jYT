d116f5ba79a872419ae2d42d52bca2717d1253dc
Jamie Law-Smith <jamielawsmith@gmail.com>
2018-04-10 15:26:02 -0700

    save data so plotting is shorter

--- deltaM_vs_beta.py
+++ deltaM_vs_beta.py
@@ -2,15 +2,20 @@ import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+from astropy.io import ascii
 import yt
 execfile('/pfs/lawsmith/jYT/my_settings.py')
+plt.rcParams['legend.fontsize'] = 14
+plt.rcParams['font.size'] = 16
+
+USE_DAT = True
 
 g = 6.67428e-8
 msun = 1.9889225e33
 rsun = 6.955e10
 
-s = 20
-lw = 1.5
+s = 30
+lw = 2
 
 ages = [
         [
@@ -36,6 +41,7 @@ ages = [
        ]
 
 labels = ['age=0Gyr', 'age=4.8Gyr', 'age=8.4Gyr']
+names = ['p0', 'p10', 'p16']
 
 class var(object):
     def __init__(self, name):
@@ -47,54 +53,65 @@ class var(object):
                 vel2 += (data[ax].v - obvec[tindex,i+3])**2.
             arr = 0.5*data['gpot'].v + 0.5*vel2
             arr[arr >= 0.] = 1.
-            arr[arr < 0.] = 0.  
-            return arr * data['cell_mass'] 
+            arr[arr < 0.] = 0.
+            return arr * data['cell_mass']
 
 fig, ax = plt.subplots()
 
-for i, age in enumerate(ages):
-    b_array = []
-    dm_array = []
-    for p in age:
-        odata = np.loadtxt('/pfs/lawsmith/FLASH4.3/runs/' + p[0] + '/pruned_sinks_evol.dat', dtype='float64')
+if USE_DAT:
+    for i, name in enumerate(names):
+        b, log_deltam_m = np.loadtxt('/pfs/lawsmith/FLASH4.3/runs/results/deltaM_vs_beta_' + names[i] + '.dat',
+        unpack=True, skiprows=1)
 
-        if odata[0,14] > odata[1,14]:
-            part_tag_pt = odata[0,0]
-        else:
-            part_tag_pt = odata[1,0]
-        odata_pt = odata[np.where(odata[:,0]==part_tag_pt)[0]]
-        odata_ob = odata[np.where(odata[:,0]!=part_tag_pt)[0]]
-        
-        ptvec = odata_pt[:,2:8]
-        obvec = odata_ob[:,2:8]
-        boundvec = obvec
-        time = odata_pt[:,1]
+        ax.plot(b, log_deltam_m, lw=lw, label=labels[i], alpha=0.75)
+        ax.scatter(b, log_deltam_m, s=s)
 
-        edata = np.loadtxt('/pfs/lawsmith/FLASH4.3/runs/' + p[0] + '/extras.dat', dtype='float64')
-        gmpt = g*edata[6]
+else:
+    for i, age in enumerate(ages):
+        b_array = []
+        dm_array = []
+        for p in age:
+            odata = np.loadtxt('/pfs/lawsmith/FLASH4.3/runs/' + p[0] + '/pruned_sinks_evol.dat', dtype='float64')
 
-        f = '/pfs/lawsmith/FLASH4.3/runs/' + p[0] + '/' + p[2]
+            if odata[0,14] > odata[1,14]:
+                part_tag_pt = odata[0,0]
+            else:
+                part_tag_pt = odata[1,0]
+            odata_pt = odata[np.where(odata[:,0]==part_tag_pt)[0]]
+            odata_ob = odata[np.where(odata[:,0]!=part_tag_pt)[0]]
 
-        pf = yt.load(f)
+            ptvec = odata_pt[:,2:8]
+            obvec = odata_ob[:,2:8]
+            boundvec = obvec
+            time = odata_pt[:,1]
 
-        tindex = abs(time - pf.current_time.v).argmin()
+            edata = np.loadtxt('/pfs/lawsmith/FLASH4.3/runs/' + p[0] + '/extras.dat', dtype='float64')
+            gmpt = g*edata[6]
 
-        myvars = var('sb_mass')
+            f = '/pfs/lawsmith/FLASH4.3/runs/' + p[0] + '/' + p[2]
 
-        yt.add_field(("gas","sb_mass"), function=myvars.mesh, take_log=False, force_override=True, units="g")
+            pf = yt.load(f)
 
-        #ad = pf.all_data()
-        #dense_ad = ad.cut_region(['obj["dens"] > 1e-11'])
+            tindex = abs(time - pf.current_time.v).argmin()
 
-        sp = pf.sphere("center", (1.0, "Mpc"))
-        dm = sp.quantities.total_quantity("sb_mass").v
+            myvars = var('sb_mass')
 
-        b_array.append(p[1])
-        dm_array.append(dm)
+            yt.add_field(("gas","sb_mass"), function=myvars.mesh, take_log=False, force_override=True, units="g")
 
-    ax.plot(b_array, np.log10(np.array(dm_array)/msun), lw=lw, label=labels[i], alpha=0.5)
-    ax.scatter(b_array, np.log10(np.array(dm_array)/msun), s=s)
+            #ad = pf.all_data()
+            #dense_ad = ad.cut_region(['obj["dens"] > 1e-11'])
 
+            sp = pf.sphere("center", (1.0, "Mpc"))
+            dm = sp.quantities.total_quantity("sb_mass").v
+
+            b_array.append(p[1])
+            dm_array.append(dm)
+
+        ax.plot(b_array, np.log10(np.array(dm_array)/msun), lw=lw, label=labels[i], alpha=0.5)
+        ax.scatter(b_array, np.log10(np.array(dm_array)/msun), s=s)
+        ascii.write([b_array, np.log10(np.array(dm_array)/msun)],
+            '/pfs/lawsmith/FLASH4.3/runs/results/deltaM_vs_beta_' + names[i] + '.dat',
+            names=['beta','log_deltaM_Msun'])
 
 ax.set_xlim(0.9, 4.1)
 ax.set_ylim(-1.5, 0.05)
@@ -102,6 +119,4 @@ ax.set_xlabel(r'$\beta$')
 ax.set_ylabel(r'$\log\ \Delta M/M_\ast$')
 ax.legend()
 fig.tight_layout()
-fig.savefig('/pfs/lawsmith/FLASH4.3/runs/results/deltaM_vs_beta.png')
-
-
+fig.savefig('/pfs/lawsmith/FLASH4.3/runs/results/deltaM_vs_beta.pdf')
