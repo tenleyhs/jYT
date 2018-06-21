d6f8ccc737b3c1e302ee1e47192a8e0083f4a24e
Jamie Law-Smith <jamielawsmith@gmail.com>
2018-06-21 16:37:54 -0700

    make all slices and movies at once

---
+++ make_slices_for_all_runs.py
@@ -0,0 +1,36 @@
+"""
+makes all the slices for a bunch of runs at once, and ideally movies too,
+so that I can just rsync it all at once to my local machine
+"""
+import os
+
+cluster = '/groups/dark/lawsmith/'
+#cluster = '/pfs/lawsmith/'
+
+dirs = [
+    'm1.0_p10_b1.5',
+    'm1.0_p10_b2.5',
+    'm1.0_p16_b1.5',
+    'm1.0_p16_b2.0',
+    'm1.0_p16_b2.5',
+    'm1.0_p16_b3.0',
+    'm1.0_p16_b3.5',
+    'm1.0_p16_b4.0',
+    'new_m1.0_p16_b4.0',
+    '43_b2.0_24k',
+    '43_b2.0_48k',
+    '43_b3.0_48k',
+]
+
+var = 'dens'
+width = 1000
+
+for dir in dirs:
+    SAVE_PATH = cluster + 'fend_slices/' + dir + '/' + var + '_' + str(width) + 'rsun/'
+    if not os.path.exists(SAVE_PATH): os.makedirs(SAVE_PATH)
+    os.system('mpirun -np 32 python ~/jYT/plot_movie.py ' + dir + ' ' + var + ' ' + str(width) + ' ' + SAVE_PATH)
+    movie_name = SAVE_PATH + dir.replace(".","_")
+    slices_names = SAVE_PATH + 'multitidal_hdf5_chk_%04d_Slice_z_dens.png'
+    os.system('ffmpeg -y -r 24 -i ' + slices_names + ' -b:v 4M ' \
+        + movie_name + '.mp4')
+    #  ' >> outputfile &'
--- plot_movie.py
+++ plot_movie.py
@@ -1,31 +1,37 @@
 '''
-mpirun -np 8 python movie.py
+run like:
+mpirun -np 32 python ~/jYT/plot_movie.py m1.0_p16_b3.0 dens 1000 SAVE_PATH
 makes a bunch of slices in parallel in prep for a movie
 then can run:
-ffmpeg -r 24 -f image2 -i multitidal_hdf5_plt_cnt_%04d_Slice_z_dens.png -b:v 4M movie.mpg
+ffmpeg -r 24 -i multitidal_hdf5_chk_%04d_Slice_z_dens.png -b:v 4M movie.mp4
+ffmpeg -r 24 -i multitidal_hdf5_plt_cnt_0%02d0_Slice_z_dens.png -b:v 4M movie.mp4
 or can use quicktime 7 like James suggested
 '''
+import yt
+import sys
 
 cluster = '/groups/dark/lawsmith/FLASH4.3_copy/runs/'
 #cluster = '/pfs/lawsmith/FLASH4.3/runs/'
 
-dir = 'm1.0_p16_b3.0'
+#dir = 'm1.0_p16_b3.0'
+dir = sys.argv[1]
 
-var = 'pres'
+#var = 'dens'
+var = sys.argv[2]
 log_tf = True
-width = (50, 'rsun')
+#width = (3, 'rsun')
+width = (int(sys.argv[3]), 'rsun')
+
 set_zlim_tf = False
 zmax = 169.88   # p1:80.78, p10:169.88, p16:500
 zmin = 1e-5 * zmax
 
-
 LOAD_FILES = cluster + dir + '/multitidal_hdf5_chk_*'
-SAVE_PATH = cluster + dir + '/' + var + '_' + str(width[0]) + 'rsun/'
+#SAVE_PATH = cluster + dir + '/' + var + '_' + str(width[0]) + 'rsun/'
+SAVE_PATH = sys.argv[4]
+#if not os.path.exists(SAVE_PATH): os.makedirs(SAVE_PATH)
 
-import yt
-#import colormaps as cmaps
 yt.enable_parallelism()
-
 ts = yt.DatasetSeries(LOAD_FILES)
 
 for ds in ts.piter():
@@ -41,6 +47,6 @@ for ds in ts.piter():
 	#s.hide_colorbar()
 	#s.set_figure_size(12)
 	#s.set_buff_size(2000)
-	#s.annotate_velocity()
+	s.annotate_velocity()
 	#s.annotate_line_integral_convolution('velx', 'vely')#, lim=(0.5,0.65))
 	s.save(SAVE_PATH)
